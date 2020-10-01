f75288074b85e2c5e3908e3fbe8ad73c3133f223
Quinton Schwagle <qschwagle@gmail.com>
2020-10-01 00:26:21 -0400

    - now detects if "Date" has been defined before modifying it - added comments

--- Copper/ResponseHeader.py
+++ Copper/ResponseHeader.py
@@ -2,6 +2,9 @@ import datetime
 
 
 class ResponseHeader:
+    """Represents a Response header.
+       set_header_line must be set at the very least before being sent
+    """
     def __init__(self):
         self.__header_fields = {}
         self.__version = ""
@@ -20,16 +23,21 @@ class ResponseHeader:
         self.__status_value = value
 
     def generate(self):
+        """Generates bytes object to be sent out to client (possibly with body)"""
         # Set the date time 
         current_time = datetime.datetime.now(tz=datetime.timezone.utc)
-        self.set_field("Date", current_time.strftime("%a, %d %b %Y %H:%M:%S GMT"))
+
+        # Check fields which can be autofilled
+        if "Date" not in self.__header_fields:
+            self.set_field("Date", current_time.strftime("%a, %d %b %Y %H:%M:%S GMT"))
 
         if "Content-Length" not in self.__header_fields:
             self.set_field("Content-Length", str(0))
 
         if "Connection" not in self.__header_fields:
             self.set_field("Connection", "closed")
-
+      
+        # render output
         out = self.__version + " " + str(self.__status_value) + " " + self.__status + "\r\n"
         for (k,v) in self.__header_fields.items():
             out += (k + ": " + v + "\r\n")
